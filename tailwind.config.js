12a8c8e4d5644c5a06962c22e346e1b5961c711b
mapas47 <gaymeryonathan@gmail.com>
2025-03-29 12:30:41 -0400

    0v

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import animations from '@midudev/tailwind-animations'
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EDBACB",
+        secondary: "#14171A",
+      },
+    },
+  },
+  plugins: [animations],
+}
